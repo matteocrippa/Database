cb33269759fc4568f0aed3491b616dfd4f1a9c8b
Matteo Crippa <matteo@boostco.de>
2017-03-24 16:09:16 +0100

    moved to class

--- Database/Classes/Database.swift
+++ Database/Classes/Database.swift
@@ -24,7 +24,7 @@ public enum DatabaseWriteType: String {
 }
 
 /// Database configuration struct
-open struct DatabaseConfiguration{
+open class DatabaseConfiguration{
   var name = ""
   var type: DatabaseWriteType = .disk
   var debug: DatabaseDebugVerbosity = .none
